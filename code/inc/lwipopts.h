fcd4e0fb75f7efc2d2fc9bf06c0f213d13c5f37f
Mike Thompson <mpthompson@gmail.com>
2017-03-29 11:29:03 -0700

    Fixed why we couldn't ping the system from Windows PC systems. It turns out that ST had broken some of the config files when they modified lwIP to support the STM32F4xx chips. The following fixes the issue so that the ICMP checksums are correctly generated by the STM32F4XX hardware and we can now ping the boards from Windows.
    See: http://lists.nongnu.org/archive/html/lwip-users/2013-07/msg00057.html
    
    Also fixed an issue with UDP packets being dropped by adjusting
    MEMP_NUM_NETBUF to a larger size.

--- code/inc/lwipopts.h
+++ code/inc/lwipopts.h
@@ -87,7 +87,7 @@
 
 /* MEMP_NUM_NETBUF: the number of struct netbufs.
  * (only needed if you use the sequential API, like api_lib.c) */
-#define MEMP_NUM_NETBUF                 2
+#define MEMP_NUM_NETBUF                 8
 
 /* MEMP_NUM_NETCONN: the number of struct netconns.  MEMP_NUM_NETCONN should be 
  * less than the sum of MEMP_NUM_{TCP,RAW,UDP}_PCB+MEMP_NUM_TCP_PCB_LISTEN
@@ -201,6 +201,8 @@
   #define CHECKSUM_GEN_UDP                0
   /* CHECKSUM_GEN_TCP==0: Generate checksums by hardware for outgoing TCP packets.*/
   #define CHECKSUM_GEN_TCP                0 
+  /* CHECKSUM_GEN_ICMP==0: Generate checksums by hardware for outgoing ICMP packets.*/
+  #define CHECKSUM_GEN_ICMP               0
   /* CHECKSUM_CHECK_IP==0: Check checksums by hardware for incoming IP packets.*/
   #define CHECKSUM_CHECK_IP               0
   /* CHECKSUM_CHECK_UDP==0: Check checksums by hardware for incoming UDP packets.*/
@@ -214,6 +216,8 @@
   #define CHECKSUM_GEN_UDP                1
   /* CHECKSUM_GEN_TCP==1: Generate checksums in software for outgoing TCP packets.*/
   #define CHECKSUM_GEN_TCP                1
+  /* CHECKSUM_GEN_ICMP==1: Generate checksums by hardware for outgoing ICMP packets.*/
+  #define CHECKSUM_GEN_ICMP               1
   /* CHECKSUM_CHECK_IP==1: Check checksums in software for incoming IP packets.*/
   #define CHECKSUM_CHECK_IP               1
   /* CHECKSUM_CHECK_UDP==1: Check checksums in software for incoming UDP packets.*/
